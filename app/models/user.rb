7001a81f01246d362afa8833b9d5c78850e9761a
Prelang Builder <builder@prelang.com>
2017-03-29 00:32:16 +0000

    Installed relation 'belongs_to' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   
   #->Prelang (user_login:devise/username_login_support)
   has_many :karteikartes
+  belongs_to :karteikarte
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
