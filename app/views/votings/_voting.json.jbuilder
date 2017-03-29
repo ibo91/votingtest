f76f06bca885cd6558d5350c357456931dd6cc12
Prelang Builder <builder@prelang.com>
2017-03-29 00:32:20 +0000

    Generated scaffolding for model 'Voting' via Rails

---
+++ app/views/votings/_voting.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! voting, :id, :einzelbewertung, :user_id, :karteikarte_id, :created_at, :updated_at
+json.url voting_url(voting, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :votings
+
   resources :fachgruppes
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
